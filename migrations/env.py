7399e98fefc83231c605835343f27deffc77b0db
Thistleman <codes5096@gmail.com>
2022-05-25 11:45:31 -0700

    Imported github workflows, fixed migration script:

--- migrations/env.py
+++ migrations/env.py
@@ -83,6 +83,7 @@ def run_migrations_online():
             connection=connection,
             target_metadata=target_metadata,
             process_revision_directives=process_revision_directives,
+            include_schemas=True,
             **current_app.extensions['migrate'].configure_args
         )
 
